ee3a272f0ac4d5cb108340c43dcf27d7b4d1d671
John <johnnaujoks@gmail.com>
2021-09-18 13:21:32 -0700

    Removing known cover preprocessing

--- main.py
+++ main.py
@@ -1,39 +1,38 @@
 import cv2
 import os
+import json
+import numpy as np
+   
+# Load in covers & covert back feature arrays
+f = open('cover_info.json')
+   
+data = json.load(f)
 
-path = 'covers'
-my_files = os.listdir(path)
-# cover1 = cv2.imread('train.png',0)
-# cover2 = cv2.imread('XMen500kb.png',0)
-# cover1_name = "X-MEN #1"
-orb = cv2.ORB_create(nfeatures=1000)
+for cover in data['cover_info']:
+    cover['desc'] = np.array(cover['desc']).astype('uint8')
+
+# Load ORB and BFM model
+orb = cv2.ORB_create(nfeatures=1250)
 bf = cv2.BFMatcher()
 
-image_info = []
-for cover in my_files:
-    filepath = path + '/' + cover
-    img_in = cv2.imread(filepath, 0)
-    kp, desc = orb.detectAndCompute(img_in, None)
-    new_dic = {'name': os.path.splitext(cover)[0],
-               'path': filepath,
-               'desc': desc,
-               'match': 0
-               }
-    image_info.append(new_dic)
-
-print(image_info)
-
+# Start capture
 cap = cv2.VideoCapture(0)
 
 while True: 
+    comic_id = ""
     success, cap_img = cap.read()
     cap_copy = cap_img.copy()
     cap_img = cv2.cvtColor(cap_img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('cap_img', cap_copy)
+    position = ((int) (cap_img.shape[1]/2 - 268/2), (int) (cap_img.shape[0]/2 - 36/2))
+
     kp, cap_desc = orb.detectAndCompute(cap_img, None)
-    for cover in image_info:
+    for cover in data['cover_info']:
         matches = bf.knnMatch(cap_desc, cover['desc'], k=2)
         good_matches = [m for m,n in matches if m.distance < .7*n.distance]
         if len(good_matches) > 12: 
             print(cover['name'])
+            comic_id = cover['name']
+    cap_copy = cv2.putText(cap_copy, comic_id, position, cv2.FONT_HERSHEY_SIMPLEX, 
+                   2, (255,255,255), 7, cv2.LINE_AA)
+    cv2.imshow('cap_img', cap_copy)        
     cv2.waitKey(1)
